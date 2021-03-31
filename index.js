ea896caceed8e0ee2c2863b1fa93bd179ffdb695
andiinho16 <anderson100oliveira@gmail.com>
2021-03-31 10:18:24 -0300

    Rodando servidor usando nodemon

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
 
-console.log(app)
+// console.log(app)
+app.get('/',(request,response)=>{
+    response.send('Olá mundo')
+})
+
+app.listen(3000,()=>{
+    console.log('Servidor Rodando.')
+})
